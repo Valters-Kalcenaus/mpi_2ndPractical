76eaec7b8a6635eff2bf902182ad63c3e6ac46aa
Valters-Kalcenaus <valters2002@gmail.com>
2023-10-01 14:43:52 +0300

    2nd practical

---
+++ 2nd_Practical_Valters_Kalcenaus/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] members = {"Valters Kalcenaus", "Viesturs Kalcenaus"};
+        boolean[] checkedItems = new boolean[members.length];
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("<No. 1> Group’s Dialog")
+                .setMultiChoiceItems(members, checkedItems, (dialog, which, isChecked) -> {
+                    String toastText = isChecked ? members[which] + " checked" : members[which] + " unchecked";
+                    Toast.makeText(this, toastText, Toast.LENGTH_SHORT).show();
+                })
+                .setPositiveButton("OK", (dialog, which) -> Toast.makeText(this, "You clicked OK", Toast.LENGTH_SHORT).show())
+                .setNegativeButton("Close", (dialog, which) -> {
+                    Toast.makeText(this, "You closed dialog", Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                });
+
+        Button dialogButton = findViewById(R.id.dialog);
+        dialogButton.setOnClickListener(v -> builder.show());
+
+        Button goToSecondButton = findViewById(R.id.GoToSecond);
+        goToSecondButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        });
+
+
+
+
+
+    }
+}
