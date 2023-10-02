2f16d256373c503a5b60e2b1bdcfd602e4868af3
Valters-Kalcenaus <valters2002@gmail.com>
2023-10-02 17:59:24 +0300

    Update MainActivity.java

--- 2nd_Practical_Valters_Kalcenaus/app/src/main/java/com/example/myapplication/MainActivity.java
+++ 2nd_Practical_Valters_Kalcenaus/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -24,24 +23,26 @@ public class MainActivity extends AppCompatActivity {
                     String toastText = isChecked ? members[which] + " checked" : members[which] + " unchecked";
                     Toast.makeText(this, toastText, Toast.LENGTH_SHORT).show();
                 })
-                .setPositiveButton("OK", (dialog, which) -> Toast.makeText(this, "You clicked OK", Toast.LENGTH_SHORT).show())
+                .setPositiveButton("OK", null)
                 .setNegativeButton("Close", (dialog, which) -> {
                     Toast.makeText(this, "You closed dialog", Toast.LENGTH_SHORT).show();
                     dialog.dismiss();
                 });
 
+        AlertDialog dialog = builder.create();
+
         Button dialogButton = findViewById(R.id.dialog);
-        dialogButton.setOnClickListener(v -> builder.show());
+        dialogButton.setOnClickListener(v -> {
+            dialog.show();
+            dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(view -> {
+                Toast.makeText(MainActivity.this, "You clicked OK", Toast.LENGTH_SHORT).show();
+            });
+        });
 
         Button goToSecondButton = findViewById(R.id.GoToSecond);
         goToSecondButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, SecondActivity.class);
             startActivity(intent);
         });
-
-
-
-
-
     }
 }
